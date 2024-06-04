2a808e155e6543af44017d0f9a42fb2ddd8e3cdf
Matheus-santos7 <matheussantosgc@icloud.com>
2024-06-04 12:26:52 -0300

    feat: using the websocket.io protocol to connect a user to the server in real time

--- src/servidor.js
+++ src/servidor.js
@@ -1,9 +1,12 @@
 // Importa o módulo Express, que é um framework para criar servidores web em Node.js
 // Importa o módulo URL para manipulação de URLs
 // Importa o módulo Path para manipulação de caminhos de arquivos e diretórios
+// Importa o módulo HTTP para criar servidores HTTP
 import express from "express";
 import url from "url";
 import path from "path";
+import http from "http";
+import { Server } from "socket.io";
 
 // Cria uma instância do Express
 // Define a porta em que o servidor irá rodar. Usa a variável de ambiente PORT se estiver definida, caso contrário usa a porta 3001.
@@ -17,7 +20,27 @@ const caminhoAtual = url.fileURLToPath(import.meta.url);
 const diretorioPublico = path.join(caminhoAtual, "../..", "public")
 app.use(express.static(diretorioPublico));
 
+// Define uma const server que cria um servidor HTTP passando a instância do Express como callback
+const server = http.createServer(app);
+
 // Inicia o servidor na porta definida e imprime uma mensagem no console quando o servidor estiver rodando
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+    console.log("A user connected");
+
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    });
+
+    socket.on("message", (message) => {
+        console.log(`Message received: ${message}`);
+        io.emit("message", message);
+    });
+});
+
+ 
