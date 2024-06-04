a6d1f21784a71b2b8592e769c99f692cbd47216c
Matheus-santos7 <matheussantosgc@icloud.com>
2024-06-04 13:38:44 -0300

    feat: customizing reason why id disconnected from socket

--- src/socket-back.js
+++ src/socket-back.js
@@ -4,8 +4,9 @@ io.on("connection", (socket) => {
     console.log(`user logged in with id: ${socket.id}`)
     socket.on;
 
-    socket.on("disconnect", () => {
-        console.log("A user disconnected");
+    socket.on("disconnect", (motivo) => {
+        console.log(`A user ${socket.id}, disconnected`);
+        motivo && console.log(`Motivo: ${motivo}`);
     });
 
     socket.on("texto_editor", (message) => {
