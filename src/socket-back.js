e62b4f6e6bd01107d420d73da91028446fce77ad
Matheus-santos7 <matheussantosgc@icloud.com>
2024-06-04 20:56:04 -0300

    refactor: refactoring in file

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,16 +1,48 @@
 import io from "./servidor.js";
 
-io.on("connection", (socket) => {
-    console.log(`user logged in with id: ${socket.id}`)
-    socket.on;
+const documentos = [
+    {
+        nome: "JavaScript",
+        texto: "texto de javascript...",
+    },
+    {
+        nome: "Node",
+        texto: "texto de node...",
+    },
+    {
+        nome: "Socket.io",
+        texto: "texto de socket.io...",
+    },
+];
 
-    socket.on("disconnect", (motivo) => {
-        console.log(`A user ${socket.id}, disconnected`);
-        motivo && console.log(`Motivo: ${motivo}`);
+io.on("connection", (socket) => {
+    console.log("Um cliente se conectou! ID:", socket.id);
+
+    socket.on("selecionar_documento", (nomeDocumento, devolverTexto) => {
+        socket.join(nomeDocumento);
+
+        const documento = encontrarDocumento(nomeDocumento);
+
+        if (documento) {
+            devolverTexto(documento.texto);
+        }
     });
 
-    socket.on("texto_editor", (message) => {
-        console.log(`Message received: ${message}`);
-        socket.broadcast.emit("message", message);
+    socket.on("texto_editor", ({ texto, nomeDocumento }) => {
+        const documento = encontrarDocumento(nomeDocumento);
+
+        if (documento) {
+            documento.texto = texto;
+
+            socket.to(nomeDocumento).emit("texto_editor_clientes", texto);
+        }
     });
 });
+
+function encontrarDocumento(nome) {
+    const documento = documentos.find((documento) => {
+        return documento.nome === nome;
+    });
+
+    return documento;
+}
