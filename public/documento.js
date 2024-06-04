464ece5c8b08d7b6a0a71db7f9e755ca5295c2a7
Matheus-santos7 <matheussantosgc@icloud.com>
2024-06-04 13:12:34 -0300

    feat: front x back event traffic using io

--- public/documento.js
+++ public/documento.js
@@ -1,1 +1,8 @@
 const socket = io();
+
+const textoEditor = document.getElementById("editor-texto");
+
+textoEditor.addEventListener("keyup", () => {
+    //console.log(textoEditor.value);
+    socket.emit("texto_editor", textoEditor.value);
+})
--- src/socket-back.js
+++ src/socket-back.js
@@ -1,13 +1,14 @@
 import io from "./servidor.js";
 
 io.on("connection", (socket) => {
-    console.log(`user logged in with id: ${socket.id}`);
+    console.log(`user logged in with id: ${socket.id}`)
+    socket.on;
 
     socket.on("disconnect", () => {
         console.log("A user disconnected");
     });
 
-    socket.on("message", (message) => {
+    socket.on("texto_editor", (message) => {
         console.log(`Message received: ${message}`);
         io.emit("message", message);
     });
